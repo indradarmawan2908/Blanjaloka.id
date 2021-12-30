02831dc53e4358566855bc32d8bc2b53924deea6
indradarmawan2908 <86046191+indradarmawan2908@users.noreply.github.com>
2021-12-30 11:36:22 +0700

    mengerjakan data produk

--- app/Http/Controllers/EditProdukController.php
+++ app/Http/Controllers/EditProdukController.php
@@ -1,54 +1,52 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\EditProduk;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
 class EditProdukController extends Controller
 {
-    public function index(){
-     $data=EditProduk::all();
-        return view('index',compact('data'));
-    }
-
-    public function back(){
-     return redirect ('/');
+    public function index()
+    {
+     $editproduk=EditProduk::all();
+        return view('editproduk.index')->with('editproduk', $editproduk);
     }
     
     public function create(){
-     return view('create');
+     return view('editproduk.create');
     }
 
     public function insert(Request $request){
-     $data=new EditProduk();
-        $data->nama_produk=$request->get('nama_produk');
-        $data->satuan=$request->get('satuan');
-        $data->harga_jual=$request->get('harga_jual');
-        $data->stok_saat_ini=$request->get('stok_saat_ini');
-        $data->stok_minimal=$request->get('stok_minimal');
-        $data->foto_produk=$request->get('foto_produk');
-        $data->save();
-     return redirect ('/');
+     $editproduk=new EditProduk();
+        $editproduk->nama_produk=$request->nama_produk;
+        $editproduk->satuan=$request->satuan;
+        $editproduk->harga_jual=$request->harga_jual;
+        $editproduk->stok_saat_ini=$request->stok_saat_ini;
+        $editproduk->stok_minimal=$request->stok_minimal;
+        $editproduk->foto_produk=$request->foto_produk;
+        $editproduk->save();
+     return redirect ('editproduk');
     }
     public function edit($kode_produk){
-        $data=EditProduk::find($kode_produk);
-        return view('edit',compact('data'));
+        $editproduk=EditProduk::find($kode_produk);
+        return view('editproduk.edit',compact('editproduk'));
        }
    
        public function update(Request $request, $kode_produk){     
-        $data = EditProduk::findOrFail($kode_produk);
-        $data->nama_produk=$request->get('nama_produk');
-        $data->satuan=$request->get('satuan');
-        $data->harga_jual=$request->get('harga_jual');
-        $data->stok_saat_ini=$request->get('stok_saat_ini');
-        $data->stok_minimal=$request->get('stok_minimal');
-        $data->foto_produk=$request->get('foto_produk');
-           $data->save();
-        return redirect ('/')->with('alert-success','Data berhasil Diubah.');
+        $editproduk = EditProduk::findOrFail($kode_produk);
+        $editproduk->nama_produk=$request->get('nama_produk');
+        $editproduk->satuan=$request->get('satuan');
+        $editproduk->harga_jual=$request->get('harga_jual');
+        $editproduk->stok_saat_ini=$request->get('stok_saat_ini');
+        $editproduk->stok_minimal=$request->get('stok_minimal');
+        $editproduk->foto_produk=$request->get('foto_produk');
+           $editproduk->save();
+        return redirect ('editproduk')->with('alert-success','Data berhasil Diubah.');
        }
     public function read($kode_produk){
-        $data=EditProduk::find($kode_produk);
-        return view('read',compact('data'));
+        $editproduk=EditProduk::find($kode_produk);
+        return view('editproduk.index',compact('editproduk'));
        }
    
 }
--- app/Models/EditProduk.php
+++ app/Models/EditProduk.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class EditProduk extends Model
 {
+    use HasFactory;
+    public $timestamps = false;
     protected $table = 'edit_produk';
     protected $primaryKey="kode_produk";
     protected $fillable = ['nama_produk','satuan','harga_jual','stok_saat_ini','stok_minimal','foto_produk'];
